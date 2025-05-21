a44eaea02f384c3d2fbadeb61eb2a7da79a30f69
Mary <vip.ivan.ivanov2003@bk.ru>
2025-05-21 15:21:42 +0300

    changed styles

--- frontend/src/popup/shared/types/distance.ts
+++ frontend/src/popup/shared/types/distance.ts
@@ -2,15 +2,3 @@ export type IPosition = {
     x:number;
     y:number;
 }
-
-export interface IDistancePopup {
-    px:number ;
-    rem:number;
-    vh:number;
-    vw:number;
-    position:IPosition;
-    firstClick:IPosition;
-    secondClick:IPosition;
-    isDragging?:boolean;
-    isVisible?:boolean;
-}
--- frontend/src/popup/widgets/Layout/ui/Logo.tsx
+++ frontend/src/popup/widgets/Layout/ui/Logo.tsx
@@ -1,9 +1,18 @@
 import styles from '../Layout.module.scss'
 
 export const Logo = () => {
+
+    const word = 'Perfect'
     return (
          <div className={styles.logo}>
-            <p><span>Perfect</span> Styles</p>
+            <p>
+                <span>
+                    {word.split("").map((char, i) => (
+                        <span key={i} className={styles.letter}>{char}</span>
+                    ))}
+                </span>
+                Styles
+            </p>
         </div>
     )
 }
